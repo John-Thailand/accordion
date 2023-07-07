8fb36cd44913f37465ff1fa9babf7f95c96f7511
John-Thailand <hamoban69@gmail.com>
2023-07-07 23:50:04 +0900

    テキストテーマについて

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  // 入力
-  const envFile = String.fromEnvironment('env');
-  await dotenv.load(fileName: envFile);
-
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text(''),
-        ),
-        body: Scaffold(
-          body: InkWell(
-            onTap: () async {
-              await Future.delayed(const Duration(seconds: 2));
-              if (context.mounted) Navigator.pop(context);
-            },
-            child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.red,
-            ),
-          ),
+          title: Text('App', style: theme.textTheme.headlineMedium),
         ),
       ),
     );
