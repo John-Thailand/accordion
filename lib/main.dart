a542589a56d4bfa471bc1f82874bce6d0f3e7dde
John-Thailand <hamoban69@gmail.com>
2023-07-08 13:06:51 +0900

    TextFieldの文字数と文字の種類を制限する

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,50 @@
-import 'package:accordion/view/theme_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() async {
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar'),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: controller,
+              keyboardType: TextInputType.text,
+              inputFormatters: [
+                // 最大8文字まで入力可能
+                LengthLimitingTextInputFormatter(8),
+                // 半角英数字のみ許可
+                FilteringTextInputFormatter.allow(
+                  RegExp(r'[a-zA-Z0-9]'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 }
